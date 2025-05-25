0132cb7cd46466dea7d6c43f8124b2792890bd78
Michał Kwiatkowski <110732078+Kfiatek430@users.noreply.github.com>
2025-05-25 23:02:21 +0200

    feat(ui): implement responsive navbar

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,73 @@
+import ThemeSwitcher from "@/components/layout/ThemeSwitcher";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "../ui/sheet";
+
+interface LinkType {
+  name: string;
+  link: string;
+}
+
+const links: LinkType[] = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Swagger",
+    link: "/swagger",
+  },
+  {
+    name: "Logger",
+    link: "/logger",
+  },
+  {
+    name: "Writer",
+    link: "/writter",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="h-16 flex justify-center bg-sidebar shadow-sm items-center w-full px-4 sticky top-0 z-50">
+      <h1 className="font-bold absolute left-5 text-xl md:text-2xl text-sidebar-foreground">
+        Wiosenna
+      </h1>
+      <div className="hidden md:flex">
+        {links.map((link: LinkType) => (
+          <Button key={link.name} asChild variant="ghost">
+            <Link key={link.name} href={link.link}>
+              {link.name}
+            </Link>
+          </Button>
+        ))}
+      </div>
+      <div className="flex gap-3 absolute right-5">
+        <ThemeSwitcher className="cursor-pointer" />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="md:hidden">
+              <MenuIcon />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="md:hidden">
+            <SheetTitle className="flex justify-center py-3">Menu</SheetTitle>
+            <div className="flex flex-col">
+              {links.map((link: LinkType) => (
+                <Button key={link.name} asChild variant="ghost">
+                  <Link key={link.name} href={link.link}>
+                    {link.name}
+                  </Link>
+                </Button>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ providers/AppProviders.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThemeProvider from "./ThemeProvider";
+
+const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default AppProviders;
