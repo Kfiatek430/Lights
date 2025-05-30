73ad4619dd0f61e8c5b4bcb1f01cd8e6973afa80
Michał Kwiatkowski <110732078+Kfiatek430@users.noreply.github.com>
2025-05-31 00:41:33 +0200

    feat(pages): implement home page with room cards grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,66 @@
+import RoomCard from "@/components/common/RoomCard";
+import { Room } from "@/types/room";
+
 export default function Home() {
-  return <div></div>;
+  const lazienka: Room = {
+    id: 1,
+    onLine: true,
+    info: {
+      name: "Łazienka parter",
+      chips: 1,
+      lines: 4,
+      patterns: 4,
+      values3b: 8,
+      patternNames: ["Zimny", "Neutralny", "Ciepły", "Relaks"],
+      lineNames: [
+        "Góra<br>zimne",
+        "Góra<br>ciepłe",
+        "Lustro<br>zimne",
+        "Lustro<br>ciepłe",
+      ],
+    },
+    motion: false,
+    motionValue: 0,
+    motionChangeTimestamp: 1748446764011,
+    pattern: 0,
+    minValue3b: 0,
+    maxValue3b: 0,
+    minValue3bReq: 0,
+    maxValue3bReq: 0,
+    lines: [
+      {
+        id: 0,
+        value3bReq: 0,
+        value3b: 0,
+        value8b: 0,
+        value16b: 0,
+      },
+      {
+        id: 1,
+        value3bReq: 0,
+        value3b: 0,
+        value8b: 2,
+        value16b: 2,
+      },
+      {
+        id: 2,
+        value3bReq: 0,
+        value3b: 0,
+        value8b: 0,
+        value16b: 0,
+      },
+      {
+        id: 3,
+        value3bReq: 0,
+        value3b: 0,
+        value8b: 2,
+        value16b: 2,
+      },
+    ],
+  };
+  return (
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-4 h-fit justify-items-center">
+      <RoomCard key={lazienka.id} room={lazienka} />
+    </div>
+  );
 }
