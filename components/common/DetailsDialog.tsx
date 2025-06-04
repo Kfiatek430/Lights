e545d3a9a2d7dfba7bace979bd4c6b962f4c5347
Michał Kwiatkowski <110732078+Kfiatek430@users.noreply.github.com>
2025-06-04 14:54:38 +0200

    feat(components): enhance details dialog layout and data

--- components/common/DetailsDialog.tsx
+++ components/common/DetailsDialog.tsx
@@ -1,6 +1,7 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -13,7 +14,9 @@ import { FC } from "react";
 import React from "react";
 import { Room } from "@/types/room";
 import { Separator } from "@/components/ui/separator";
-import LineComponent from "./LineComponent";
+import LineViewComponent from "./LineViewComponent";
+import { cn } from "@/lib/utils";
+import { PATTERNS } from "@/lib/constants";
 
 interface DetailsDialogProps {
   room: Room;
@@ -26,52 +29,82 @@ const DetailsDialog: FC<DetailsDialogProps> = ({ room }) => {
         <Button variant="outline">Szczegóły</Button>
       </DialogTrigger>
       <DialogContent showCloseButton={false}>
-        <DialogHeader className="-mt-3 -mx-6 border-b pb-3 px-6 flex flex-row justify-between items-center bg-sidebar">
-          <DialogTitle>{room.info.name}</DialogTitle>
+        <DialogHeader className="-mt-3 -mx-6 border-b pb-3 px-6 flex flex-row justify-between items-center">
+          <DialogTitle className="text-xl">{room.info.name}</DialogTitle>
+          <DialogDescription className="sr-only">
+            Szczegóły pokoju {room.info.name}
+          </DialogDescription>
           <DialogPrimitive.Close
-            className={buttonVariants({
-              variant: "outline",
-              size: "icon",
-            })}
+            className={cn(
+              buttonVariants({
+                variant: "outline",
+                size: "icon",
+              }),
+              "cursor-pointer"
+            )}
           >
             <X />
           </DialogPrimitive.Close>
         </DialogHeader>
 
         <div className="flex flex-col justify-center items-center gap-8">
-          <div className="w-full flex flex-row justify-center gap-3 text-xs">
-            <Button
-              variant="destructive"
-              className="flex-1 h-fit flex flex-col gap-2"
-            >
-              <LightbulbOff />
-              <p>Wyłącz</p>
-            </Button>
-            <Button variant="blue" className="flex-1 h-fit flex flex-col gap-2">
-              <Pointer />
-              <p>Przyciski</p>
-            </Button>
-            <Button
-              variant="success"
-              className="flex-1 h-fit flex flex-col gap-2"
-            >
-              <Lightbulb />
-              Włącz
-            </Button>{" "}
+          <div className="w-full flex flex-col gap-3">
+            <h4 className="font-semibold text-lg">Przyciski sterowania</h4>
+            <div className="w-full flex flex-row justify-center gap-3 text-xs">
+              <Button
+                variant="destructive"
+                className="flex-1 h-fit flex flex-col gap-2"
+              >
+                <LightbulbOff />
+                <p>Wyłącz</p>
+              </Button>
+              <Button
+                variant="blue"
+                className="flex-1 h-fit flex flex-col gap-2"
+              >
+                <Pointer />
+                <p>Przyciski</p>
+              </Button>
+              <Button
+                variant="success"
+                className="flex-1 h-fit flex flex-col gap-2"
+              >
+                <Lightbulb />
+                Włącz
+              </Button>
+            </div>
           </div>
-          <Slider
-            value={[0]}
-            max={7}
-            className="w-full mt-4"
-            staticThumb={4}
-          />{" "}
-          <div className="w-full flex flex-col justify-center items-center gap-3">
-            {room.lines.map((line, index) => (
-              <React.Fragment key={index}>
-                <LineComponent line={line} name={room.info.lineNames[index]} />
-                <Separator />
-              </React.Fragment>
-            ))}
+          <Separator />
+          <div className="w-full flex flex-col items-end gap-3">
+            <h4 className="w-full text-start font-semibold text-lg">
+              Pomiar Jasności
+            </h4>
+            <Slider
+              value={[room.maxValue3b]}
+              max={7}
+              staticThumb={room.minValue3b}
+              disabled={true}
+            />
+          </div>
+          <Separator />
+          <div className="w-full flex flex-row gap-2">
+            <h4 className="font-semibold text-lg">Wzorzec oświetlenia:</h4>
+            <p className="text-lg">{PATTERNS[room.pattern]}</p>
+          </div>
+          <Separator />
+          <div className="w-full flex flex-col gap-3">
+            <h4 className="font-semibold text-lg">Punkty świetlne</h4>
+            <div className="w-full flex flex-col justify-center items-center gap-3">
+              {room.lines.map((line, index) => (
+                <React.Fragment key={index}>
+                  <LineViewComponent
+                    line={line}
+                    name={room.info.lineNames[index]}
+                  />
+                  <Separator />
+                </React.Fragment>
+              ))}
+            </div>
           </div>
         </div>
       </DialogContent>
