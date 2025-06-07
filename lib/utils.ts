af0867a29cf8a092c3e38685ddf5fe81473161a1
Michał Kwiatkowski <110732078+Kfiatek430@users.noreply.github.com>
2025-06-07 22:53:48 +0200

    feat(utils): add mergeObjects utility and clean up constants

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Line, Room } from "@/types";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function mergeObjects(source: any, target: any) {
+  const merged = { ...target };
+  for (const key in source) {
+    if (key === "rooms") {
+      source[key].forEach((room: Room) => {
+        merged[key][room.id] = mergeObjects(room, merged[key][room.id]);
+      });
+    } else if (key === "lines") {
+      source[key].forEach((line: Line) => {
+        merged[key][line.id] = mergeObjects(line, merged[key][line.id]);
+      });
+    } else {
+      merged[key] = source[key];
+    }
+  }
+
+  return merged;
 }
