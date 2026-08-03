6fe29ef5b4d86b758ffb946494a7e3f3a9a9f3ca
Kfiatek430 <michal.kwiatkowski.430@gmail.com>
2026-08-03 10:26:11 +0200

    feat(house): add data hooks for alarm, power and value control

--- hooks/useHouseWebSocket.ts
+++ hooks/useHouseWebSocket.ts
@@ -3,16 +3,33 @@
 import { Client } from "@stomp/stompjs";
 import { useEffect } from "react";
 
-export function useHouseWebSocket(onMessage: Function) {
+export type HouseWsEvent =
+  | { type: "connected" }
+  | { type: "disconnected" }
+  | { type: "message"; bytes: number; serverTimestamp: number };
+
+export function useHouseWebSocket(
+  onMessage: Function,
+  onEvent?: (event: HouseWsEvent) => void,
+) {
   useEffect(() => {
     const client = new Client({
       brokerURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/houseWS`,
+      reconnectDelay: 5000,
       onConnect: () => {
+        onEvent?.({ type: "connected" });
         client.subscribe("/topic/house", (message) => {
           const newHouse = JSON.parse(message.body);
           onMessage(newHouse);
+          onEvent?.({
+            type: "message",
+            bytes: message.body.length,
+            serverTimestamp: newHouse.timestamp,
+          });
         });
       },
+      onDisconnect: () => onEvent?.({ type: "disconnected" }),
+      onWebSocketClose: () => onEvent?.({ type: "disconnected" }),
     });
 
     client.activate();
